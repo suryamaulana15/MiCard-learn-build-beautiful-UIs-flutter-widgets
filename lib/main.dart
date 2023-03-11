800815f7060379540b2a4c2bae0b1e0b6a952081
surya maulana <suryamaulana@Tris-MacBook-Pro.local>
2023-03-11 16:20:39 +0700

    module container widget single child

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            margin:
+                const EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
+            color: Colors.red,
+            child: Text('Hello'),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
