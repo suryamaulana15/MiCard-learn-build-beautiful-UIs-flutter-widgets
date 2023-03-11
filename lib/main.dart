518df7bd3ceed3e2d91e7287fad36c205f3c8a4e
surya maulana <suryamaulana@Tris-MacBook-Pro.local>
2023-03-11 22:27:57 +0700

    fixed module layouting

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,75 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            margin:
-                const EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
-            color: Colors.red,
-            child: Text('Hello'),
-          ),
-        ),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/surya.jpg'),
+            ),
+            Text(
+              'Surya Maulana',
+              style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'SOFTWARE ENGINERING',
+              style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  '+02 123 456 789',
+                  style: TextStyle(
+                    color: Colors.teal.shade900,
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'suryamaulana@gmail.com',
+                  style: TextStyle(
+                    color: Colors.teal.shade900,
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            )
+          ],
+        )),
       ),
     );
   }
